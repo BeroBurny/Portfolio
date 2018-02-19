cf4a265c79938d33da1f7137df6ff7c5ec471103
Bernard Stojanović <bero4net@gmail.com>
2018-02-19 18:55:12 +0100

    Template prototype
    First template prototype.

--- js/script.js
+++ js/script.js
@@ -7,4 +7,6 @@ window.addEventListener('resize', sysHeight);
 function sysHeight(){
 	const h = document.getElementsByClassName("about-text")[0].clientHeight;
 	if(h) document.getElementsByClassName("potrait-space")[0].style.height = h + "px";
+	for (let project of document.getElementsByClassName("project")) project.style.height = (project.clientWidth * 0.5) + "px";
+	for (let picture of document.getElementsByClassName("picture")) picture.style.height = (picture.clientWidth * 0.66) + "px";
 }
