c91ca1b8d9c44e76932b2a8319aa72aa68ba7553
Bernard Stojanović <bero4net@gmail.com>
2018-02-21 20:09:02 +0100

    Image Update
    New images updated, and some small adjustments.

--- js/script.js
+++ js/script.js
@@ -8,5 +8,5 @@ function sysHeight(){
 	const h = document.getElementsByClassName("about-text")[0].clientHeight;
 	if(h) document.getElementsByClassName("potrait-space")[0].style.height = h + "px";
 	for (let project of document.getElementsByClassName("project")) project.style.height = (project.clientWidth * 0.5) + "px";
-	for (let picture of document.getElementsByClassName("picture")) picture.style.height = (picture.clientWidth * 0.66) + "px";
+	for (let picture of document.getElementsByClassName("picture")) picture.style.height = (picture.clientWidth * 0.5625) + "px";
 }
