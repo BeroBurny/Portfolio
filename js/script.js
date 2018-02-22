012e20f28ba621175fbbcf170420b4f41cf0b2e3
Bernard Stojanović <bero4net@gmail.com>
2018-02-22 13:50:11 +0100

    .
    Update "Finish" page

--- js/script.js
+++ js/script.js
@@ -5,8 +5,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 window.addEventListener('resize', sysHeight);
 
 function sysHeight(){
-	const h = document.getElementsByClassName("about-text")[0].clientHeight;
-	if(h) document.getElementsByClassName("potrait-space")[0].style.height = h + "px";
-	for (let project of document.getElementsByClassName("project")) project.style.height = (project.clientWidth * 0.5) + "px";
+	if(window.innerWidth > 1300){
+		const h = document.getElementsByClassName("about-text")[0].clientHeight;
+		if(h) document.getElementsByClassName("potrait-space")[0].style.height = h + "px";
+	} else document.getElementsByClassName("potrait-space")[0].style.height = "auto";
 	for (let picture of document.getElementsByClassName("picture")) picture.style.height = (picture.clientWidth * 0.5625) + "px";
 }
